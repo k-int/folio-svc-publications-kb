f1486cd0783302882992d0223e2d1584010ec018
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-09-07 10:05:38 +0100

    updated

---
+++ pubskb/grails-app/domain/pubskb/IdentifierNamespace.groovy
@@ -0,0 +1,7 @@
+package pubskb
+
+class IdentifierNamespace {
+
+    static constraints = {
+    }
+}
---
+++ pubskb/src/test/groovy/pubskb/IdentifierNamespaceSpec.groovy
@@ -0,0 +1,18 @@
+package pubskb
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class IdentifierNamespaceSpec extends Specification implements DomainUnitTest<IdentifierNamespace> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
