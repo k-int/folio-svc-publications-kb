da3423da2755bfdfc017a8c94c0968b5006ce56e
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-09-13 10:07:58 +0100

    Added hash

--- pubskb/grails-app/domain/pubskb/Instance.groovy
+++ pubskb/grails-app/domain/pubskb/Instance.groovy
@@ -1,14 +1,21 @@
 package pubskb
 
+
+/**
+ * An occurrence of a work.
+ * The important item here is the discriminator. Discriminators are used to ensure a unique hash at instance creation time.
+ */
 class Instance {
 
   Work work
   String title
+  String discriminator
   RefdataValue itemType // BKM-Books and monographs / SER - Series / etc
 
   static constraints = {
      work (nullable:true);
      title (nullable:false, blank:false)
+     discriminator (nullable:true);
      itemType (nullable:true);
   }
 
